9000abf2cdd8db9f456a1c81269d9982e6288d25
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-09-16 22:07:50 +0530

    commit message

---
+++ Basics of Programming/Loops/Triangle (right angle).java
@@ -0,0 +1,18 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                System.out.print("*" +" ");
+            }
+             System.out.println();
+        }
+       
+    }
+}
