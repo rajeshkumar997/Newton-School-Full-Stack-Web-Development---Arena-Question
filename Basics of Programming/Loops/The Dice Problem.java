70ca40cb90508f54847ae8c0de7951f46c437213
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-09-16 21:34:44 +0530

    commit message

---
+++ Basics of Programming/Loops/The Dice Problem.java
@@ -0,0 +1,17 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int opposite = 7-n;
+        System.out.println(opposite);
+    }
+    }
+}
