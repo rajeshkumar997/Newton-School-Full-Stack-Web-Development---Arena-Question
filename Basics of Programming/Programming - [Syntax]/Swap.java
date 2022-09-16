e8601e8e5ecf27efbc7be7124eb2d06f63c5c839
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-09-16 22:12:01 +0530

    commit message

---
+++ Basics of Programming/Programming - [Syntax]/Swap.java
@@ -0,0 +1,13 @@
+// A: contains first value
+// B: contains second value
+// You need to just swap the values
+
+static void Swap(){
+//Enter your code here  
+// int A=5;
+// int B=7;
+
+int temp = A;
+A=B;
+B=temp;
+}
