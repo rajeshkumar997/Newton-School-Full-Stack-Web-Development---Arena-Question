99a65865a7f03a7eab1aae4ed0fa5ab8f74aac8b
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-09-16 20:46:11 +0530

    commit message

---
+++ Basics of Programming/Variables & Operators/Simple Input- String.java
@@ -0,0 +1,20 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+        // String S;
+        // Scanner sc = new Scanner(System.in);
+        // S = sc.nextLine();
+        // System.out.println(S);
+
+        Scanner myObj = new Scanner(System.in);
+        String userName;
+        //System.out.println("Enter username");
+        userName = myObj.nextLine();
+         System.out.println(userName);
+    }
+}
