6c4d20ffb566e06d69ed4725ca0bbeba0d354ac6
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-09-16 21:38:40 +0530

    commit message

---
+++ Basics of Programming/Loops/5 stars.java
@@ -0,0 +1,15 @@
+static void vertical5(){
+//Enter your code here
+int n=5;
+for(int i=1; i<=n; i++){
+    System.out.println("*");
+}
+}
+
+static void horizontal5(){
+//Enter your code here
+int n=5;
+for(int i=1; i<=n; i++){
+    System.out.print("*"+ " ");
+}
+}
