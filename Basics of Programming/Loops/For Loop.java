d0a528a35eea83d2838e4a42e058a7e4843fd477
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-09-16 21:32:40 +0530

    commit message

---
+++ Basics of Programming/Loops/For Loop.java
@@ -0,0 +1,11 @@
+public static void For_Loop(int n){
+//Enter your code here
+for(int i=1;i<=n;i++){
+if(i%2==0){
+    System.out.print("even" + " ");
+}
+else{
+    System.out.print("odd"+ " ");
+}
+}
+}
