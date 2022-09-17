63232d3165f64622e90ac660fae55dd04e512c14
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-09-17 20:05:15 +0530

    commit message

---
+++ Linked List/Double Linked List/Palindrome in Doubly Linked List.java
@@ -0,0 +1,23 @@
+/*
+class Node
+{
+   char data;
+   Node next, prev;
+};
+*/
+ static boolean isPalin( Node left)
+    {
+        // Write your code here
+        Node right = left;
+        while(right.next != null){
+           right = right.next;
+        }
+        while(left != right){
+           if(left.data != right.data){
+              return false;
+           }
+           left = left.next;
+           right = right.prev;
+         }
+         return true;
+    }
